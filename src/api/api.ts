1f31390f9e3e019d9ec264ba4b402d315d98c39c
Sulthanzuza <sulthanpriv@gmail.com>
2025-08-12 19:12:05 +0530

    updated url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://concal-4he6.onrender.com/api';
+const API_BASE_URL = 'https://concal-q66g.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
